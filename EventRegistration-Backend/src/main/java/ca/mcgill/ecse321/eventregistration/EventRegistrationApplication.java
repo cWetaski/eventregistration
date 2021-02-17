c551da576393d4af430cae990ea8550faf60b655
cWetaski <charles.wetaski@mail.mcgill.ca>
2021-02-17 15:15:00 -0500

    Re init spring with Java version 1.8

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -1,21 +1,13 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
 @SpringBootApplication
 public class EventRegistrationApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(EventRegistrationApplication.class, args);
-  }
-
-  @RequestMapping("/")
-  public String greeting(){
-    return "Hello world!";
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
 
 }
